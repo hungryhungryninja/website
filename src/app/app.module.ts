e389bd9c07fec979cfb6ffd47b1ce38d55a790ca
Greg Brewer <greg@gabrewer.com>
2016-12-02 19:15:21 -0700

    Initial auth implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { ButtonsModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { AUTH_PROVIDERS } from 'angular2-jwt';
 
 import { IngredientService } from './ingredients/ingredient.service'; 
 
 import { AppComponent } from './app.component';
+import { HomeComponent }       from './home.component';
+import { routing,
+         appRoutingProviders } from './app.routes';
+
 import { IngredientFormComponent } from './ingredients/ingredient-form.component';
 import { IngredientListComponent } from './ingredients/ingredient-list.component';
 
@@ -15,17 +20,24 @@ import { IngredientListComponent } from './ingredients/ingredient-list.component
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     IngredientFormComponent,
     IngredientListComponent
   ],
+  
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     AlertModule,
-    ButtonsModule
+    ButtonsModule,
+    routing
   ],
-  providers: [IngredientService],
+  providers: [
+        appRoutingProviders,
+        AUTH_PROVIDERS,
+       IngredientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
